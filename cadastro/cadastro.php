ba4b3d61dcf577c11b3f62dd746573c4a51cfd71
De-Bart <debart1995@gmail.com>
2025-10-29 20:38:04 -0300

    Correção Back-end

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -29,7 +29,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../cadastro/cadastro.css">
+    <link rel="stylesheet" href="cadastro.css">
     <title>Cadastre-se</title>
 </head>
 <body>
