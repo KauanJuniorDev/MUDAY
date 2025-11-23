ba43d36a8fc1523746cee11636849478f7fc4b3e
KauanJuniorDev <kauan.silva.28alunos@gmail.com>
2025-11-23 20:25:34 -0300

    Update database connection details in config.php

--- cadastro/config.php
+++ cadastro/config.php
@@ -1,9 +1,10 @@
 <?php
 
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "muday";
+    $db_host = "sql300.infinityfree.com";
+    $db_user = "if0_39417832";
+    $db_pass = "KAnes2025";
+    $db_name = "if0_39417832_muday_db";
+	$port = (3306);
 
     $conexao = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
